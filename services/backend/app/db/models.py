29a2c098fd16686143e7506d6e074c20481152ac
IliaAzhel <aje-ilia2013@yandex.by>
2020-11-20 19:30:06 +0300

    added datamodel

--- services/backend/app/db/models.py
+++ services/backend/app/db/models.py
@@ -1,6 +1,33 @@
 from .db import db
 
-class Movie(db.Document):
-    name = db.StringField(required=True, unique=True)
-    casts = db.ListField(db.StringField(), required=True)
-    genres = db.ListField(db.StringField(), required=True)
+class User(db.Document):
+    email = db.StringField(max_length=255)
+    name = db.StringField(max_length=255)
+    surname = db.StringField(max_length=255)
+    password = db.StringField(max_length=255)
+    type = db.StringField(max_length=255)
+    orders_status = db.ListField(db.ReferenceField(Status), default=[])
+    customer = db.StringField(max_length=255)
+
+class Status(db.Documents):
+	name = db.StringField(max_length=255)
+	status = db.StringField(max_length=255)
+
+class Order(db.Document):
+    author = db.StringField(max_length=255)
+    title = db.StringField(max_length=255)
+    description = db.StringField(max_length=255)
+    dateCreate = db.StringField(max_length=255)
+    counter = db.IntField(default = 0)
+    data_type = db.StringField(max_length=255)
+    data = db.ListField(db.ReferenceField(Data), default=[])
+
+class Data(db.Documents):
+	valueObj = db.StringField(max_length=255)
+	res = db.ListField(db.ReferenceField(Result), default=[])
+
+class Result(db.Documents):
+	answer = db.StringField(max_length=255)
+    counter = db.IntField(default = 0)
+
+	
