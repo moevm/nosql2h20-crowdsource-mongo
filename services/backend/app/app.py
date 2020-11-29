282ce123590e8d37f7fc0b6a366c788d51587838
Alexsandr Komarov <31541802+godor1333@users.noreply.github.com>
2020-11-29 19:00:10 +0300

    update app after_request add

--- services/backend/app/app.py
+++ services/backend/app/app.py
@@ -33,5 +33,15 @@ app.register_blueprint(admin)
 def check_flask():
     return "Flask is ok"
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
+
+
+
 if __name__ == '__main__':
     app.run ()
